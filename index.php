13f082bc4bfba77b2df27bd0755a4a53ed14ce63
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-28 14:33:45 -0500

    Création page index + styles.css

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>★Clapii★</title>
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+    <h1>Bienvenue!<h1>
+</body>
+</html>
