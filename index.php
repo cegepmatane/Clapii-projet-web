9899a1b9f704a6f3cde3d3aa2847cd37b7222e8e
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-30 15:26:16 -0500

    Affichage basique film + tooltip

--- index.php
+++ index.php
@@ -3,21 +3,37 @@
 
     include "Ressources/Connexion.php";
 
-    $REQUETE_SQL_TEST = "SELECT * FROM Film WHERE id = 1";
+    $REQUETE_SQL_TEST = "SELECT * FROM Film";
     $requeteTest = $basededonnees->prepare($REQUETE_SQL_TEST);
     $requeteTest->execute();
-    $film = $requeteTest->fetch();
-    print_r($film);
+    $films = $requeteTest->fetchAll();
+    
 ?>
 
 
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>★Clapii★</title>
+    <link rel="stylesheet" href="Ressources/styles.css">
+</head>
+
+
+<body>
+<header>
+    <h1>Bienvenue!</h1>
+</header>
 <div>
-
-    <h1>Bienvenue!<h1>
-    
-<div>
-
-
+    <ul>
+        <?php foreach($films as $film){
+            //print_r($films["id"]);
+            echo("<li class=\"tooltip\"><img class=\"thumbnail\" src=\"Ressources/images/PlaceHolder.jpg\"> <span class=\"tooltiptext\">" . $film['nom'] . "</span></li>");
+        }
+        ?>    
+    </ul>
+</div>
 <?php
     include "footer.php";
     ?>
+</body>
+</html>
