ce5c742d9046f5e6ac9594faef635d2a4afb6598
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-01-31 15:53:53 -0500

    systeme de cartes

--- index.php
+++ index.php
@@ -1,14 +1,111 @@
 <?php
-    include "./Ressources/header.php";
+include "./Ressources/header.php";
 
-    
 
 ?>
+<div class="container">
+    <div class="row">
 
-<header>
-    <h1>Bienvenue!</h1>
-</header>
+        <div class="col s4">
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Les derniers films</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col s4">
+
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Les films les mieux notés</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col s4">
+
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Les films les plus mal notés</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+    <div class="row">
+
+        <div class="col s4">
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Les films les plus commentés</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col s4">
+
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Les derniers commentaires</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col s4">
+
+
+            <div class="card grey lighten-4">
+                <div class="card-content brown-text text-darken-3">
+                    <span class="card-title">Top commentaires</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Plus de résultats</a>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
 
 <?php
-    include "./Ressources/footer.php";
+include "./Ressources/footer.php";
 ?>
