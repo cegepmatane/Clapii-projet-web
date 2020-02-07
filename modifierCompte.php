36d1b6c35c166db31e152de6e799e518c9ac7a9a
ClementineAllard <60235896+ClementineAllard@users.noreply.github.com>
2020-02-06 21:24:48 -0500

    Page modification compte

---
+++ modifierCompte.php
@@ -0,0 +1,60 @@
+<?php
+include "./Ressources/header.php";
+?>
+
+<div class="container">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s3">
+                <img src="Ressources/images/PlaceHolder.jpg" alt="" class="circle responsive-img">
+
+                <div class="row">
+
+                    <div class="col s3">
+                    </div>
+                        <div class="col s5">
+                            <a class="btn waves-effect waves-light">Modifier</a>
+                        </div>
+                            <div class="col s2">
+                            </div></div>
+            </div>
+                <form class="col s7">
+                    <div class="row">
+                        <div class="input-field col s7">
+                            <input value="$pseudo" id="first_name2" type="text" class="validate">
+                            <label class="active" for="first_name2">Pseudo</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s7">
+                            <input value="$mail" id="first_name" type="text" class="validate">
+                            <label class="active" for="first_name">Mail</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s7">
+                            <input value="$mot de passe" id="first_name" type="text" class="validate">
+                            <label class="active" for="first_name">Mot de passe</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s7">
+                            <input value="$mot de passe" id="first_name" type="text" class="validate">
+                            <label class="active" for="first_name">Mot de passe</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s7">
+                            <input value="$langue" id="first_name" type="text" class="validate">
+                            <label class="active" for="first_name">Langue</label>
+                        </div>
+                    </div>
+                </form>
+            <div class="col s2">
+                <a class="waves-effect waves-light btn-large">ok</a>
+            </div>
+        </div>
+    </div>
+</div>
+
