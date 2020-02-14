eca1315c34b79a207ee23f18e7c2db895dc405fc
Clement D <54323202+Caribou77@users.noreply.github.com>
2020-02-14 15:44:51 -0500

    ajout container inscription/connexion

--- inscription_connexion.php
+++ inscription_connexion.php
@@ -1,74 +1,79 @@
 <?php
 include "./Ressources/header.php"; ?>
-    <div class="row">
-        <form class="col s6">
+    <div class=container>
 
-            <div class="row">
-                <div class="col s6">
-                    <label for="pseudo">Pseudo</label>
-                    <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
-                </div>
+        <div class="row">
+            <form class="col s6">
 
-                <div class="row">
-                    <div class="col s6">
-                        <label for="password">Mot de passe</label>
-                        <input id="password" type="password" class="validate">
+                <div class="card-panel grey lighten-5 z-depth-1">
+
+                    <div class="row">
+                        <div class="col s6">
+                            <label for="pseudo">Pseudo</label>
+                            <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
+                        </div>
+
+                        <div class="col s6">
+                            <label for="password">Mot de passe</label>
+                            <input id="password" type="password" class="validate">
+                        </div>
+
+
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Se connecter
+                            <i class="material-icons right">send</i>
+                        </button>
+
                     </div>
                 </div>
+            </form>
 
 
-                <button class="btn waves-effect waves-light" type="submit" name="action">Se connecter
-                    <i class="material-icons right">send</i>
-                </button>
+            <form class="col s6">
 
-            </div>
+                <div class="card-panel grey lighten-5 z-depth-1">
+                    <div class="row">
+                        <div class="col s6">
+                            <label for="pseudo">Pseudo</label>
+                            <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
+                        </div>
+
+                        <div class="col s6">
+                            <label for="email">Email</label>
+                            <input id="email" type="email" class="validate">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">
+                            <label for="first_name">Prénom</label>
+                            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                        </div>
 
 
+                        <div class="col s6">
+                            <label for="last_name">Nom</label>
+                            <input id="last_name" type="text" class="validate">
+                        </div>
+                    </div>
 
-        </form>
+                    <div class="row">
+                        <div class="col s6">
+                            <label for="password">Mot de passe</label>
+                            <input id="password" type="password" class="validate">
+                        </div>
+                        <div class="col s6">
+                            <label for="password">Confirmation mot de passe</label>
+                            <input id="comfirm-password" type="password" class="validate">
+                        </div>
+                    </div>
 
-        <form class="col s6">
 
-            <div class="row">
-                <div class="col s6">
-                    <label for="pseudo">Pseudo</label>
-                    <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">S'inscrire
+                        <i class="material-icons right">send</i>
+                    </button>
                 </div>
-
-                <div class="col s6">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="validate">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s6">
-                    <label for="first_name">Prénom</label>
-                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                </div>
-
-
-                <div class="col s6">
-                    <label for="last_name">Nom</label>
-                    <input id="last_name" type="text" class="validate">
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col s6">
-                    <label for="password">Mot de passe</label>
-                    <input id="password" type="password" class="validate">
-                </div>
-                <div class="col s6">
-                    <label for="password">Confirmation mot de passe</label>
-                    <input id="comfirm-password" type="password" class="validate">
-                </div>
-            </div>
-
-
-            <button class="btn waves-effect waves-light" type="submit" name="action">S'inscrire
-                <i class="material-icons right">send</i>
-            </button>
-        </form>
+            </form>
+        </div>
+      
     </div>
 
 <?php
