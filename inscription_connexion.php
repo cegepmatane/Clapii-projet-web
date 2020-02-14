ff9262c4c2d9f9a0923645b96b5c913a8dc50512
Clement D <54323202+Caribou77@users.noreply.github.com>
2020-02-14 14:31:15 -0500

    Page inscription-connexion

--- inscription_connexion.php
+++ inscription_connexion.php
@@ -1,47 +1,76 @@
-<div class="row">
-    <form class="col s12">
+<?php
+include "./Ressources/header.php"; ?>
+    <div class="row">
+        <form class="col s6">
 
-
-        <div class="row">
-            <div class="input-field col s6">
-                <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                <label for="first_name">First Name</label>
-            </div>
-
-
-            <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
-                <label for="last_name">Last Name</label>
-            </div>
-        </div>
-
-
-        </div>
-
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
-                <label for="password">Password</label>
-            </div>
             <div class="row">
-                <div class="input-field col s12">
-                    <input id="comfirm-password" type="password" class="validate">
-                    <label for="password">Password</label>
+                <div class="col s6">
+                    <label for="pseudo">Pseudo</label>
+                    <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
                 </div>
 
-        </div>
+                <div class="row">
+                    <div class="col s6">
+                        <label for="password">Mot de passe</label>
+                        <input id="password" type="password" class="validate">
+                    </div>
+                </div>
 
 
-        <div class="row">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Se connecter
+                    <i class="material-icons right">send</i>
+                </button>
 
-            <div class="input-field col s12">
-                <input id="email" type="email" class="validate">
-                <label for="email">Email</label>
             </div>
 
 
-        </div>
+
+        </form>
+
+        <form class="col s6">
+
+            <div class="row">
+                <div class="col s6">
+                    <label for="pseudo">Pseudo</label>
+                    <input placeholder="Placeholder" id="pseudo" type="text" class="validate">
+                </div>
+
+                <div class="col s6">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="validate">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s6">
+                    <label for="first_name">Prénom</label>
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                </div>
 
 
-    </form>
-</div>
+                <div class="col s6">
+                    <label for="last_name">Nom</label>
+                    <input id="last_name" type="text" class="validate">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col s6">
+                    <label for="password">Mot de passe</label>
+                    <input id="password" type="password" class="validate">
+                </div>
+                <div class="col s6">
+                    <label for="password">Confirmation mot de passe</label>
+                    <input id="comfirm-password" type="password" class="validate">
+                </div>
+            </div>
+
+
+            <button class="btn waves-effect waves-light" type="submit" name="action">S'inscrire
+                <i class="material-icons right">send</i>
+            </button>
+        </form>
+    </div>
+
+<?php
+include "./Ressources/footer.php";
+?>
