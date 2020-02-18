3e8fddd22b10138ee329b554dbd8dbdf6a0c7894
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-02-18 14:02:58 -0500

    creation modele

--- administration/SQL/Clapii.sql
+++ administration/SQL/Clapii.sql
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `casting` (
-  `id` int(11) NOT NULL,
-  `Nom` int(11) NOT NULL,
-  `Prenom` int(11) NOT NULL
+                           `id` int(11) NOT NULL,
+                           `Titre` int(11) NOT NULL,
+                           `Prenom` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -41,8 +41,8 @@ CREATE TABLE `casting` (
 --
 
 CREATE TABLE `casting_film` (
-  `id_film` int(11) NOT NULL,
-  `id_casting` int(11) NOT NULL
+                                `id_film` int(11) NOT NULL,
+                                `id_casting` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -52,8 +52,8 @@ CREATE TABLE `casting_film` (
 --
 
 CREATE TABLE `categorie` (
-  `id` int(11) NOT NULL,
-  `nom_categorie` varchar(255) NOT NULL
+                             `id` int(11) NOT NULL,
+                             `nom_categorie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -63,8 +63,8 @@ CREATE TABLE `categorie` (
 --
 
 CREATE TABLE `categorie_film` (
-  `id_film` int(11) NOT NULL,
-  `id_categorie` int(11) NOT NULL
+                                  `id_film` int(11) NOT NULL,
+                                  `id_categorie` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -74,10 +74,13 @@ CREATE TABLE `categorie_film` (
 --
 
 CREATE TABLE `film` (
-  `id` int(11) NOT NULL,
-  `titre` varchar(255) NOT NULL,
-  `date_sortie` date NOT NULL,
-  `synopsis` text NOT NULL
+                        `id` int(11) NOT NULL,
+                        `titre` varchar(255) NOT NULL,
+                        `date_sortie` date NOT NULL,
+                        `synopsis` text NOT NULL,
+                        `origine` text NOT NULL,
+                        `genre` text NOT NULL,
+                        `duree` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -85,14 +88,14 @@ CREATE TABLE `film` (
 --
 
 INSERT INTO `film` (`id`, `titre`, `date_sortie`, `synopsis`) VALUES
-(1, 'Film test', '2020-01-27', 'Ce film est un test pour établir la connexion entre l\'affichage client (page web) et le serveur.'),
-(2, 'Sonic the Test', '2020-02-14', 'SANIC'),
-(3, 'Fast and Furious 69', '2069-04-20', 'Maintenant avec 100% moins de rapide, et 600% plus de dangereux, bienvenue dans l&#39;espace. YADESVOITUREMAGIQUE'),
-(4, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '2020-01-22', 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'),
-(5, 'Nom modifié', '2020-01-01', 'Synopsis changé'),
-(6, 'Matrix', '2333-02-02', 'Synopsis du film test'),
-(11, 'Filtre film', '2000-01-01', 'Test des PHP filters'),
-(12, 'Forest gump', '2020-01-01', 'Maman disait toujours la vie c&#39;est comme une boite de chocolat on ne sait jamais sur quoi on va tomber.');
+(1, 'Film test', '2020-01-27', 'Ce film est un test pour établir la connexion entre l\'affichage client (page web) et le serveur.',NULL,NULL,NULL),
+(2, 'Sonic the Test', '2020-02-14', 'SANIC',NULL,NULL,NULL),
+(3, 'Fast and Furious 69', '2069-04-20', 'Maintenant avec 100% moins de rapide, et 600% plus de dangereux, bienvenue dans l&#39;espace. YADESVOITUREMAGIQUE',NULL,NULL,NULL),
+(4, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '2020-01-22', 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',NULL,NULL,NULL),
+(5, 'Titre modifié', '2020-01-01', 'Synopsis changé',NULL,NULL,NULL),
+(6, 'Matrix', '2333-02-02', 'Synopsis du film test',NULL,NULL,NULL),
+(11, 'Filtre film', '2000-01-01', 'Test des PHP filters',NULL,NULL,NULL),
+(12, 'Forest gump', '2020-01-01', 'Maman disait toujours la vie c&#39;est comme une boite de chocolat on ne sait jamais sur quoi on va tomber.',NULL,NULL,NULL);
 
 --
 -- Index pour les tables déchargées
@@ -102,33 +105,33 @@ INSERT INTO `film` (`id`, `titre`, `date_sortie`, `synopsis`) VALUES
 -- Index pour la table `casting`
 --
 ALTER TABLE `casting`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `casting_film`
 --
 ALTER TABLE `casting_film`
-  ADD KEY `id_film` (`id_film`),
-  ADD KEY `id_casting` (`id_casting`);
+    ADD KEY `id_film` (`id_film`),
+    ADD KEY `id_casting` (`id_casting`);
 
 --
 -- Index pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `categorie_film`
 --
 ALTER TABLE `categorie_film`
-  ADD KEY `id_film` (`id_film`),
-  ADD KEY `id_categorie` (`id_categorie`);
+    ADD KEY `id_film` (`id_film`),
+    ADD KEY `id_categorie` (`id_categorie`);
 
 --
 -- Index pour la table `film`
 --
 ALTER TABLE `film`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -138,19 +141,19 @@ ALTER TABLE `film`
 -- AUTO_INCREMENT pour la table `casting`
 --
 ALTER TABLE `casting`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Contraintes pour les tables déchargées
@@ -160,20 +163,20 @@ ALTER TABLE `film`
 -- Contraintes pour la table `casting_film`
 --
 ALTER TABLE `casting_film`
-  ADD CONSTRAINT `Casting_Film_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Casting_Film_ibfk_2` FOREIGN KEY (`id_casting`) REFERENCES `casting` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `Casting_Film_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `Casting_Film_ibfk_2` FOREIGN KEY (`id_casting`) REFERENCES `casting` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  ADD CONSTRAINT `categorie_ibfk_1` FOREIGN KEY (`id`) REFERENCES `categorie_film` (`id_categorie`);
+    ADD CONSTRAINT `categorie_ibfk_1` FOREIGN KEY (`id`) REFERENCES `categorie_film` (`id_categorie`);
 
 --
 -- Contraintes pour la table `categorie_film`
 --
 ALTER TABLE `categorie_film`
-  ADD CONSTRAINT `categorie_film_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`);
+    ADD CONSTRAINT `categorie_film_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
