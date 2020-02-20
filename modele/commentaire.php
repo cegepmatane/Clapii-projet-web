b09825dace7e8fd871166ccb3b47a7c920ff8f3f
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-02-20 16:29:32 -0500

    modele commentaire et utilisateur

--- modele/commentaire.php
+++ modele/commentaire.php
@@ -3,5 +3,89 @@
 
 class commentaire
 {
+    protected $id;
+    protected $id_utilisateur;
+    protected $id_film;
+    protected $text;
+
+    /**
+     * commentaire constructor.
+     * @param $id
+     * @param $id_utilisateur
+     * @param $id_film
+     * @param $text
+     */
+    public function __construct($id, $id_utilisateur, $id_film, $text)
+    {
+        $this->id = $id;
+        $this->id_utilisateur = $id_utilisateur;
+        $this->id_film = $id_film;
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUtilisateur()
+    {
+        return $this->id_utilisateur;
+    }
+
+    /**
+     * @param mixed $id_utilisateur
+     */
+    public function setIdUtilisateur($id_utilisateur): void
+    {
+        $this->id_utilisateur = $id_utilisateur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdFilm()
+    {
+        return $this->id_film;
+    }
+
+    /**
+     * @param mixed $id_film
+     */
+    public function setIdFilm($id_film): void
+    {
+        $this->id_film = $id_film;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
 
 }
--- modele/utilisateur.php
+++ modele/utilisateur.php
@@ -3,5 +3,118 @@
 
 class utilisateur
 {
+    protected $id;
+    protected $pseudo;
+    protected $nom;
+    protected $prenom;
+    protected $mail;
+    protected $password;
+
+    public function __construct(int $id, string $pseudo, string $nom, string $prenom, string $mail, string $password){
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->mail = $mail;
+        $this->password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPseudo(): string
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param string $pseudo
+     */
+    public function setPseudo(string $pseudo): void
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail(string $mail): void
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
 
 }
