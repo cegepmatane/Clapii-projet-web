819d592bf3f8566e9f8975461842a369e3a59fdb
Clement D <54323202+Caribou77@users.noreply.github.com>
2020-02-25 15:12:57 -0500

    Completer CommentaireSQL

---
+++ phpsql/CommentaireSQL.php
@@ -0,0 +1,10 @@
+<?php
+interface CommentaireSQL
+{
+    public const SELECT_ALL_COMMENTAIRE = "SELECT * FROM commentaire";
+    public const SELECT_COMMENTAIRE_BY_ID = "SELECT * FROM commentaire WHERE id = :id";
+    public const SELECT_COMMENTAIRE_BY_ID_UTILISATEUR = "SELECT * FROM commentaire WHERE id_utilisateur = :id_utilisateur";
+    public const SELECT_FILM_BY_ID_FILM = "SELECT * FROM commentaire WHERE id_film = :id_film";
+    public const INSERT_COMMENTAIRE_BY_ID_ID_UTILISATEUR_ID_FILM_TEXT = "INSERT INTO commentaire(id, id_utilisateur, id_film, text) VALUES (:id, :id_utilisateur, :id_film, :text)";
+    public const SELECT_FILM_BY_ID_UTILISATEUR_ID_FILM = "SELECT * FROM commentaire WHERE id_film = :id_film AND id_utilisateur = :id_utilisateur";
+}
