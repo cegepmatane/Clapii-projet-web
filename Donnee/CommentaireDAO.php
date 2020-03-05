2466104ec566a8775e0c84db50dd7081e188b0d2
Clement D <54323202+Caribou77@users.noreply.github.com>
2020-03-05 18:08:12 -0500

    CommentairesDAO

--- Donnee/CommentaireDAO.php
+++ Donnee/CommentaireDAO.php
@@ -6,12 +6,44 @@ include_once "./Donnee/BaseDeDonnees.php";
 class CommentaireDAO implements CommentaireSQL
 {
 
-    public static function recupererCommentaireParIdUtilisateur($idUtilisateur){
+    public static function recupererCommentaireParIdUtilisateur($idUtilisateur, $commentaire)
+    {
+
+        $connexion = BaseDeDonnees::getInstance()->getConnexion();
+        $demandeCommentaire = $connexion->prepare(self::SELECT_COMMENTAIRE_BY_ID_UTILISATEUR);
+        $demandeCommentaire->bindParam(':id_utilisateur', $idUtilisateur, PDO::PARAM_INT);
+        $demandeCommentaire->execute();
+
+        $commentaires = $demandeCommentaire->fetchALL(PDO::FETCH_ASSOC);
+
+        for ($i = o; $i < count($commentaires); $i++) {
+            $listeCommentaire[$i] = new commentaire($commentaires[$i]['id'],
+                $commentaires['idUtilisateur'],
+                $commentaires['idFilm'],
+                $commentaires['text']);
+        }
+        return $listeCommentaire;
 
     }
 
-    public static function recupererCommentaireParIdFilm($idFilm){
+    public static function recupererCommentaireParIdFilm($idFilm)
+    {
 
+        $connexion = BaseDeDonnees::getInstance()->getConnexion();
+        $demandeCommentaire = $connexion->prepare(self::SELECT_COMMENTAIRE_BY_ID_FILM);
+        $demandeCommentaire->bindParam(':id_film', $idFilm, PDO::PARAM_INT);
+        $demandeCommentaire->execute();
+
+        $commentaires = $demandeCommentaire->fetchALL(PDO::FETCH_ASSOC);
+
+
+        for ($i = o; $i < count($commentaires); $i++) {
+            $listeCommentaire[$i] = new commentaire($commentaires[$i]['id'],
+                $commentaires['idUtilisateur'],
+                $commentaires['idFilm'],
+                $commentaires['text']);
+        }
+        return new $listeCommentaire;
     }
-
 }
+
