4c4ac221c60703ce1dabb659635ae6d0c584d4f4
Raphaël Lavoie <rapolysec@hotmail.com>
2020-03-06 14:44:05 -0500

    Oops, changement transaction -> achat

--- PhpSQL/TransactionSQL.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface TransactionSQL
-{
-    public const SELECT_ALL_TRANSACTION = "SELECT * FROM transaction";
-    public const INSERT_TRANSACTION_BY_UTILISATEUR_DATE = "INSERT INTO transaction(idUtilisateur, date) VALUES (:idUtilisateur, :date)";
-
-}
