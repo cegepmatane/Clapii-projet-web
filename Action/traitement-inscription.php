695c6f8e973b093ba601b5644803792061c53703
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-04-06 11:20:45 -0400

    pour clement

--- Action/traitement-inscription.php
+++ Action/traitement-inscription.php
@@ -1,37 +1,35 @@
 <?php
 
-    
-    include "../Ressources/header.php";
-    include "./Donnee/UtilisateurDAO.php";
+session_start();
+
+include "./Donnee/UtilisateurDAO.php";
 
 
-    $filtresUtilisateur = 
-	array(
-		'id' => FILTER_VALIDATE_INT,
-		'pseudo' => FILTER_SANITIZE_STRING,
-		'nom' => FILTER_SANITIZE_STRING,
+$filtresUtilisateur =
+    array(
+        'id' => FILTER_VALIDATE_INT,
+        'pseudo' => FILTER_SANITIZE_STRING,
+        'nom' => FILTER_SANITIZE_STRING,
         'prenom' => FILTER_SANITIZE_STRING,
         'mail' => FILTER_SANITIZE_STRING,
         'password' => FILTER_SANITIZE_STRING,
-		
+
     );
-    
-    $utilisateur = filter_input_array(INPUT_POST, $filtresUtilisateur);
-    $objetUtilisateur = new Utilisateur(1,$utilisateur['pseudo'],
+
+$utilisateur = filter_input_array(INPUT_POST, $filtresUtilisateur);
+$objetUtilisateur = new Utilisateur(1, $utilisateur['pseudo'],
     $utilisateur['nom'],
     $utilisateur['prenom'],
-    $utilisateur['mail'], 
+    $utilisateur['mail'],
     $utilisateur['password']);
-    UtilisateurDAO::insererUtilisateur($objetUtilisateur);
-    ?>
-    <header><h1>Vous êtes maintenant inscrit, <?=$objetUtilisateur->getPseudo()?>!</h1></header>
+UtilisateurDAO::insererUtilisateur($objetUtilisateur);
 
+$parametres = [];
 
+$parametres['inscription'] = true;
 
+http_redirect('../inscription_connexion.php', $parametres);
 
-
-<a href="films.php" class="brown lighten-2 btn"><i class="material-icons left">arrow_back</i>Retour</a>
-
-<?php
-    include "../Ressources/footer.php";
 ?>
+
+
