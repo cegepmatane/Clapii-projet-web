e919ce38c50c74ef2f587c116333a2f557d37e9a
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-04-09 15:00:24 -0400

    fix liste des achats dans panneau admin
    si la liste etait vide , une érreur était affiché

--- Administration/Donnee/AchatDAO.php
+++ Administration/Donnee/AchatDAO.php
@@ -1,36 +1,40 @@
 <?php
-include_once __DIR__."/../PhpSQL/AchatSQL.php";
-include_once __DIR__."/../Modele/Achat.php";
-include_once __DIR__."/../Donnee/BaseDeDonnees.php";
+include_once __DIR__ . "/../PhpSQL/AchatSQL.php";
+include_once __DIR__ . "/../Modele/Achat.php";
+include_once __DIR__ . "/../Donnee/BaseDeDonnees.php";
 
 class AchatDAO implements AchatSQL
-	{
-		public static function listerAchats()
-		{
+{
+    public static function listerAchats()
+    {
 
-			$connexion = BaseDeDonnees::getInstance()->getConnexion();
+        $connexion = BaseDeDonnees::getInstance()->getConnexion();
 
-			$demandeAchats = $connexion->prepare(self::SELECT_ALL_ACHAT);
-			$demandeAchats->execute();
-            $AchatsTableau = $demandeAchats->fetchAll(PDO::FETCH_ASSOC);
+        $demandeAchats = $connexion->prepare(self::SELECT_ALL_ACHAT);
+        $demandeAchats->execute();
+        $achatsTableau = $demandeAchats->fetchAll(PDO::FETCH_ASSOC);
 
-            for($i = 0; $i < count($AchatsTableau); $i++) {
-                $Achats[$i] = new Achat($AchatsTableau[$i]['id'],
-                $AchatsTableau[$i]['id_utilisateur'],
-                $AchatsTableau[$i]['date']);
-            }
-			return $Achats;
-		}
+        $achats = [];
 
-		public static function insererAchat($achat)
-        {
-            $connexion = BaseDeDonnees::getInstance()->getConnexion();
-            $demandeAchat = $connexion->prepare(self::INSERT_ACHAT_BY_UTILISATEUR_DATE);
-            $idUtilisateur = $achat->getIdUtilisateur();
-            $date = $achat->getDate();
-            $demandeAchat->bindParam(':id_utilisateur', $idUtilisateur, PDO::PARAM_INT);
-            $demandeAchat->bindParam(':date', $date, PDO::PARAM_STR);
-			$demandeAchat->execute();
+        for ($i = 0; $i < count($achatsTableau); $i++) {
+            $achats[$i] = new Achat($achatsTableau[$i]['id'],
+                $achatsTableau[$i]['id_utilisateur'],
+                $achatsTableau[$i]['date']);
         }
-	}
+        return $achats;
+
+    }
+
+    public static function insererAchat($achat)
+    {
+        $connexion = BaseDeDonnees::getInstance()->getConnexion();
+        $demandeAchat = $connexion->prepare(self::INSERT_ACHAT_BY_UTILISATEUR_DATE);
+        $idUtilisateur = $achat->getIdUtilisateur();
+        $date = $achat->getDate();
+        $demandeAchat->bindParam(':id_utilisateur', $idUtilisateur, PDO::PARAM_INT);
+        $demandeAchat->bindParam(':date', $date, PDO::PARAM_STR);
+        $demandeAchat->execute();
+    }
+}
+
 ?>
