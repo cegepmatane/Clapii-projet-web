6b48182c4291924d7be643f92b7efbb9cebc2a0d
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-04-10 12:48:36 -0400

    lisibilité du code

--- Action/recuperer-details-film.php
+++ Action/recuperer-details-film.php
@@ -3,13 +3,17 @@
 include "../Donnee/FilmDAO.php";
 
 if (isset($_GET['idFilm'])) {
+    
     $idFilm = $_GET['idFilm'];
+
     $film = FilmDAO::detaillerFilm($idFilm);
+
     echo "<film id='" . $film->getId() . "'>" .
         "<titre>" . $film->getTitre() . "</titre>" .
         "<date>" . $film->getDate() . "</date>" .
         "<synopsis>" . $film->getSynopsis() . "</synopsis>" .
         "</film>";
+
 } else {
     echo "id invalide";
 }
