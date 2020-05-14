c1c4f9510c9ad526a22f1347250ae477b95a2348
Clement D <54323202+Caribou77@users.noreply.github.com>
2020-05-13 22:49:26 -0400

    ajax clement fonctionnel

--- inscription_connexion.php
+++ inscription_connexion.php
@@ -27,6 +27,29 @@ if (isset($_SESSION['id'])) {
                 <button class="btn waves-effect waves-light" onclick="requetteConnexion()">Se connecter
                     <i class="material-icons right">send</i>
                 </button>
+
+
+            </div>
+        </div>
+
+
+        <div id="modalConnexion" class="modal">
+            <div class="modal-content">
+                <h4>Bon retour sur notre site</h4>
+                <p>Vous etes connecté</p>
+            </div>
+            <div class="modal-footer">
+                <a href="index.php" class="modal-close waves-effect waves-green btn-flat">Continuer</a>
+            </div>
+        </div>
+
+        <div id="modalEchecConnexion" class="modal">
+            <div class="modal-content">
+                <h4>La connexion a échoué</h4>
+                <p>Vérifier vos identifiants</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Réessayer</a>
             </div>
         </div>
 
@@ -81,23 +104,54 @@ if (isset($_SESSION['id'])) {
 
 <script>
 
+
+
     function requetteConnexion() {
+
         var pseudo = document.getElementById('cpseudo').value;
         var password = document.getElementById('cpassword').value;
+
         console.log(pseudo);
         console.log(password);
 
         var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
         xhttp.open("POST", "Action/traitement-connexion.php", true);
+
         xhttp.onreadystatechange = function() {
-            if (xhttp.readyState>3 && xhttp.status==200) {
-                console.log(xhttp.responseText);
+            if (xhttp.readyState>3 && xhttp.status === 200) {
+
+                var connexionXML = xhttp.responseText.replace('bool(false)', '');
+
+                afficherModal(connexionXML);
 
             }
         };
+
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send("pseudo="+pseudo+"&password="+password);
+    }
 
+    function afficherModal(connexionXML) {
+        console.log(connexionXML);
+
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(connexionXML, "text/xml");
+        console.log(xmlDoc);
+
+        var etat = xmlDoc.getElementsByTagName("etat")[0].childNodes[0].nodeValue;;
+        console.log("etat de la requette : " + etat);
+        if(parseInt(etat) == 1){
+            var modal = document.getElementById("modalConnexion");
+
+        }else{
+            var modal = document.getElementById("modalEchecConnexion");
+        }
+
+        var instance = M.Modal.getInstance(modal);
+        instance.open();
     }
 
 </script>
+<?php
+include "./Ressources/footer.php";
+?>
